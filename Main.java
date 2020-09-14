3a13ff1dc7de891977b1104688aabc63e23306fa
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-14 14:59:45 +0000

    Finished modulus practice

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+   Scanner input = new Scanner(System.in);
+   // Ask for integers
+   System.out.println("Please enter in two integers, on seperate lines, to divide.:");
+   // declaring numerator and denominator variables
+    int numerator = input.nextInt();
+    int denominator = input.nextInt();
+    // declaring quotient and remainder variables
+    int quotient = numerator/denominator;
+    int remainder = numerator%denominator;
+    // stating the answer
+    System.out.println(quotient + " with a of remainder " + remainder);
   }
 }
